6fa4d366b8ad4e706455b075832a38b3a849c6a8
YanivZimmer <zimmer.yaniv@gmail.com>
2017-12-26 15:36:18 +0200

    commit before pull

--- client/cmake-build-debug/lib/googletest-release-1.8.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yanivzi/CLionProjects/GitHubAdvancedProg/AdvancedProg/client/lib/googletest-release-1.8.0/googlemock
-# Build directory: /home/yanivzi/CLionProjects/GitHubAdvancedProg/AdvancedProg/client/cmake-build-debug/lib/googletest-release-1.8.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
